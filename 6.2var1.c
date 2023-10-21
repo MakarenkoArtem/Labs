63052448341e1117e9d727cc5f81eed9c1fdf05c
MakarenkoArtem <74790725+MakarenkoArtem@users.noreply.github.com>
2023-10-21 21:23:33 +0300

    Update 6.2var1.c

--- 6.2var1.c
+++ 6.2var1.c
@@ -7,32 +7,51 @@
 
 #define OK 0
 #define ErrorIncorrectInput "Incorrect input"
+#define TypeOfMatix int
+#ifdef TypeOfMatix==int 
+#define TOMFConsole "%d" //TOMFConsole-TypeOfMatixForConsole
+#elif TypeOfMatix == float || TypeOfMatix==double
+#define TOMFConsole "%f"
+#elif TypeOfMatix == unsigned int
+#define TOMFConsole "%u"
+#elif TypeOfMatix == char
+#define TOMFConsole "%c"
+#elif TypeOfMatix == long long int
+#define TOMFConsole "%lld"
+#elif TypeOfMatix == long int
+#define TOMFConsole "%ld"
+#endif
 
 int inputColRow(int* rows, int* columns);
 void** createMatrix(int columns, int rows);
 void* mallocList(int count, int sizeOfType);
 void* callocList(int count, int sizeOfType);
-int inputMatrix(int** matrix, int columns, int rows);
-int solutionFunc(int** matrix, int columns, int rows);
-int printMatrix(int** matrix, int columns, int rows);
-void freeMatrix(int** matrix, int rows);
+int inputMatrix(TypeOfMatix** matrix, int columns, int rows);
+TypeOfMatix* maxElem(TypeOfMatix** matrix, int columns, int rows);
+TypeOfMatix sumOnDiagonal(TypeOfMatix** matrix, int columns, int rows);
+int solutionFunc(TypeOfMatix** matrix, int columns, int rows);
+int printMatrix(TypeOfMatix** matrix, int columns, int rows);
+void freeMatrix(TypeOfMatix** matrix, int rows);
+int outputText(char* s);
+
 int main() {
 	int rows, columns;
 	if (inputColRow(&rows, &columns)) {
 		outputText(ErrorIncorrectInput);
 		return !OK;
 	}
-	int** matrix = (int**)createMatrix(columns, rows);
+	TypeOfMatix** matrix = (TypeOfMatix**)createMatrix(columns, rows);
 	inputMatrix(matrix, columns, rows);
 	solutionFunc(matrix, columns, rows);
 	printMatrix(matrix, columns, rows);
 	freeMatrix(matrix, rows);
+	_getch();
 	return OK;
 }
 
 int inputColRow(int* rows, int* columns) {
 	printf("Enter rows and columns: ");
-	return scanf("%i %i", rows, columns) <2 || *rows <= 0 || *columns <= 0;
+	return scanf("%i %i", rows, columns) < 2 || *rows <= 0 || *columns <= 0;
 }
 
 void* mallocList(int count, int sizeOfType) {
@@ -44,17 +63,20 @@ void* callocList(int count, int sizeOfType) {
 }
 
 void** createMatrix(int columns, int rows) {
-	int** matrix = (int*)callocList(rows, sizeof(int*));
+	//1)sizeof(void*)-size all types addresses equal
+	//2)(TypeOfMatix**)-in define changes the type
+	//3)callocList-in C++ you only need to change the body of the callocList()/mallocList()
+	TypeOfMatix** matrix = (TypeOfMatix**)callocList(rows, sizeof(void*));
 	for (int i = 0; i < rows; ++i) {
-		matrix[i] = (int*)callocList(columns, sizeof(int));
+		matrix[i] = (TypeOfMatix*)callocList(columns, sizeof(TypeOfMatix));
 	}
-	return matrix;
+	return (void**)matrix;
 }
 
-int inputMatrix(int** matrix, int columns, int rows){
+int inputMatrix(TypeOfMatix** matrix, int columns, int rows) {
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < columns; ++j) {
-			while (scanf("%i", &matrix[i][j]) == 0) {
+			while (scanf(TOMFConsole, &matrix[i][j]) == 0) {
 				printf("Value not number\n");
 				scanf("%*[^\r^\n]");
 			}
@@ -63,44 +85,44 @@ int inputMatrix(int** matrix, int columns, int rows){
 	return OK;
 }
 
-void swap(int* adres1, int* adres2) {
-	*adres1 = *adres1 + *adres2;
-	*adres2 = *adres1 - *adres2;
-	*adres1 = *adres1 - *adres2;
-}
-
-int solutionFunc(int** matrix, int columns, int rows) {
-	int x = 0, y = 0, maxValue = matrix[0][0], sum = 0;
-	for (int i = 0; i < rows - 1; ++i) {
-		for (int j = columns - 2 - i; j > -1; --j) {
+TypeOfMatix sumOnDiagonal(TypeOfMatix** matrix, int columns, int rows) {
+	TypeOfMatix maxValue = matrix[0][0], sum = 0;
+	for (int i = rows - 1; i > -1; --i) {
+		for (int j = 0; j < rows - i - 1; ++j) {
 			sum += matrix[i][j];
 		}
 	}
-
+	return sum;
+}
+TypeOfMatix* maxElem(TypeOfMatix** matrix, int columns, int rows) {
+	TypeOfMatix* maxElement = matrix[0] + 0;
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < columns; ++j) {
-			if (maxValue < matrix[i][j]) {
-				maxValue = matrix[i][j];
-				x = i;
-				y = j;
+			if (*maxElement < matrix[i][j]) {
+				maxElement = matrix[i] + j;
 			}
 		}
 	}
-	matrix[x][y] = sum;
+	return maxElement;
+}
+int solutionFunc(TypeOfMatix** matrix, int columns, int rows) {
+	TypeOfMatix* maxElement = maxElem(matrix, columns, rows);
+	*maxElement = sumOnDiagonal(matrix, columns, rows);
 	return OK;
 }
 
-int printMatrix(int** matrix, int columns, int rows) {
+int printMatrix(TypeOfMatix** matrix, int columns, int rows) {
 	for (int i = 0; i < rows; ++i) {
 		printf("\n");
 		for (int j = 0; j < columns; ++j) {
-			printf("%i ", matrix[i][j]);
+			printf(TOMFConsole" ", matrix[i][j]);
 		}
 	}
 	return OK;
 }
 
-void freeMatrix(int** matrix, int rows){
+
+void freeMatrix(TypeOfMatix** matrix, int rows) {
 	for (int i = 0; i < rows; ++i) {
 		free(matrix[i]);
 	}
