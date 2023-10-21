1274a3fd32fa8869068e4b5d3fe4532c77b25449
MakarenkoArtem <74790725+MakarenkoArtem@users.noreply.github.com>
2023-10-21 21:13:02 +0300

    Update 6.1.c

--- 6.1.c
+++ 6.1.c
@@ -7,28 +7,47 @@
 
 #define OK 0
 #define ErrorIncorrectInput "Incorrect input"
+#define TypeOfMatix int
+#ifdef TypeOfMatix==int 
+	#define TOMFConsole "%d" //TOMFConsole-TypeOfMatixForConsole
+#elif TypeOfMatix == float || TypeOfMatix==double
+	#define TOMFConsole "%f"
+#elif TypeOfMatix == unsigned int
+	#define TOMFConsole "%u"
+#elif TypeOfMatix == char
+	#define TOMFConsole "%c"
+#elif TypeOfMatix == long long int
+	#define TOMFConsole "%lld"
+#elif TypeOfMatix == long int
+	#define TOMFConsole "%ld"
+#endif
 
 int inputCount(int* count);
-void** createMatrix(int columns, int rows);
+void** createMatrix(int columns, int rows, int size);
 void* mallocList(int count, int sizeOfType);
 void* callocList(int count, int sizeOfType);
-int inputMatrix(int** matrix, int columns, int rows);
-int solutionFunc(int** matrix, int count);
-int printMatrix(int** matrix, int columns, int rows);
-void freeMatrix(int** matrix, int rows);
+int inputMatrix(TypeOfMatix** matrix, int columns, int rows);
+void swap(TypeOfMatix* address1, TypeOfMatix* address2);
+int solutionFunc(TypeOfMatix** matrix, int count);
+int printMatrix(TypeOfMatix** matrix, int columns, int rows);
+void freeMatrix(TypeOfMatix** matrix, int rows);
+int outputText(char* s);
+
 int main() {
 	int count;
 	if (inputCount(&count)) {
 		outputText(ErrorIncorrectInput);
 		return !OK;
 	}
-	int** matrix = (int**)createMatrix(count, count);
+	TypeOfMatix** matrix = (TypeOfMatix**)createMatrix(count, count, sizeof(TypeOfMatix));
 	inputMatrix(matrix, count, count);
 	solutionFunc(matrix, count);
 	printMatrix(matrix, count, count);
 	freeMatrix(matrix, count);
+	_getch();
 	return OK;
 }
+
 int inputCount(int* count) {
 	printf("Enter count: ");
 	return scanf("%i", count) == 0 || *count <= 0;
@@ -43,17 +62,20 @@ void* callocList(int count, int sizeOfType) {
 }
 
 void** createMatrix(int columns, int rows) {
-	int** matrix = (int*)callocList(rows, sizeof(int*));
+	//1)sizeof(void*)-size all types addresses equal
+	//2)(TypeOfMatix**)-in define changes the type
+	//3)callocList-in C++ you only need to change the body of the callocList()/mallocList()
+	TypeOfMatix** matrix = (TypeOfMatix**)callocList(rows, sizeof(void*));
 	for (int i = 0; i < rows; ++i) {
-		matrix[i] = (int*)callocList(columns, sizeof(int));
+		matrix[i] = (TypeOfMatix*)callocList(columns, sizeof(TypeOfMatix));
 	}
-	return matrix;
+	return (void**)matrix;
 }
 
-int inputMatrix(int** matrix, int columns, int rows) {
+int inputMatrix(TypeOfMatix** matrix, int columns, int rows) {
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < columns; ++j) {
-			while (scanf("%i", &matrix[i][j]) == 0) {
+			while (scanf(TOMFConsole, &matrix[i][j]) == 0) {
 				printf("Value not number\n");
 				scanf("%*[^\r^\n]");
 			}
@@ -62,32 +84,33 @@ int inputMatrix(int** matrix, int columns, int rows) {
 	return OK;
 }
 
-void swap(int* adres1, int* adres2) {
-	*adres1 = *adres1 + *adres2;
-	*adres2 = *adres1 - *adres2;
-	*adres1 = *adres1 - *adres2;
+void swap(TypeOfMatix* address1, TypeOfMatix* address2) {
+	TypeOfMatix dop = *address1;
+	*address1 = *address2;
+	*address2 = dop;
 }
 
-int solutionFunc(int** matrix, int count) {
-	for (int i = 0; i < count - 1; ++i) {
-		for (int j = count - 2 - i; j > -1; --j) {
-			swap(&matrix[i][j], &matrix[count - 1 - j][count - 1 - i]);
+int solutionFunc(TypeOfMatix** matrix, int count) {
+	count--;
+	for (int i = 0; i < count; ++i) {
+		for (int j = count - 1 - i; j > -1; --j) {
+			swap(&matrix[i][j], &matrix[count - j][count - i]);
 		}
 	}
 	return OK;
 }
 
-int printMatrix(int** matrix, int columns, int rows) {
+int printMatrix(TypeOfMatix** matrix, int columns, int rows) {
 	for (int i = 0; i < rows; ++i) {
 		printf("\n");
 		for (int j = 0; j < columns; ++j) {
-			printf("%i ", matrix[i][j]);
+			printf(TOMFConsole" ", matrix[i][j]);
 		}
 	}
 	return OK;
 }
 
-void freeMatrix(int** matrix, int rows) {
+void freeMatrix(TypeOfMatix** matrix, int rows) {
 	for (int i = 0; i < rows; ++i) {
 		free(matrix[i]);
 	}
