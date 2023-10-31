7cabf147efbd56b1e6dea86271a9b74e6b26c72c
MakarenkoArtem <74790725+MakarenkoArtem@users.noreply.github.com>
2023-10-31 18:48:18 +0300

    Create 8.1.c

---
+++ 8.1.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include "stdafx.h"
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+#include"libraries\strbase.h"
+
+#define OK 0
+
+
+int outputText(char* s);
+int solutionFunc(char* str) {
+	int i = 0;
+	while (str[i] != '\0') {
+		if (str[i] == ' ') {
+			if (i == 0 || str[i+1]==' ' || str[i + 1] == '\0' || str[i + 1] == ','
+				|| str[i + 1] == '!' || str[i + 1] == '.' || str[i + 1] == '?') {
+				moveStr(str, i);
+				continue;
+			}
+		}
+		i++;
+	}
+	return OK;
+}
+int main() {
+	char str[] = "";
+	gets(str);
+	outputText("|");
+	outputText(str);
+	outputText("|\n|");
+	solutionFunc(str);
+	outputText(str);
+	outputText("|");
+	_getch();
+	return OK;
+}
+
+
+int outputText(char* s) {
+	printf(s);
+	return OK;
+}
