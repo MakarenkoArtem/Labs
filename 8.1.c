afbb02d793cf95026dcb53e8106785374b301f0d
MakarenkoArtem <74790725+MakarenkoArtem@users.noreply.github.com>
2023-10-31 18:52:40 +0300

    Update 8.1.c

--- 8.1.c
+++ 8.1.c
@@ -8,22 +8,9 @@
 
 #define OK 0
 
-
 int outputText(char* s);
-int solutionFunc(char* str) {
-	int i = 0;
-	while (str[i] != '\0') {
-		if (str[i] == ' ') {
-			if (i == 0 || str[i+1]==' ' || str[i + 1] == '\0' || str[i + 1] == ','
-				|| str[i + 1] == '!' || str[i + 1] == '.' || str[i + 1] == '?') {
-				moveStr(str, i);
-				continue;
-			}
-		}
-		i++;
-	}
-	return OK;
-}
+int solutionFunc(char* str);
+
 int main() {
 	char str[] = "";
 	gets(str);
@@ -37,6 +24,20 @@ int main() {
 	return OK;
 }
 
+int solutionFunc(char* str) {
+	int i = 0;
+	while (str[i] != '\0') {
+		if (str[i] == ' ') {
+			if (i == 0 || str[i + 1] == ' ' || str[i + 1] == '\0' || str[i + 1] == ','
+				|| str[i + 1] == '!' || str[i + 1] == '.' || str[i + 1] == '?') {
+				moveStr(str, i);
+				continue;
+			}
+		}
+		i++;
+	}
+	return OK;
+}
 
 int outputText(char* s) {
 	printf(s);
