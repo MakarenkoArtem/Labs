dd578a8f46cd13c0bdf8daa9615cbb64c0b728f8
MakarenkoArtem <74790725+MakarenkoArtem@users.noreply.github.com>
2023-11-19 21:12:55 +0300

    Update 8.1.c

--- 8.1.c
+++ 8.1.c
@@ -5,45 +5,68 @@
 #include<math.h>
 #include<stdlib.h>
 #include"libraries\mystring.h"
+#include <locale.h>
+
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
 
 #define OK 0
-#define ErrorIncorrectInput "Incorrect input"
+#define Error "Error"
 #define MemoryError "Memory error"
-#define OneSpaceRight " ,!.?-)"
+#define ErrorIncorrectInput "Incorrect input"
+#define OneSpaceRight ",!.?-)"
 #define OneSpaceLeft "("
 
+
+void* reallocList(void* str, int count, int sizeOfType);
+void* mallocList(int count, int sizeOfType);
 int outputText(char* s);
-int solutionFunc(char* str);
+void freeTwoDArr(char** matrix, int rows);
+void freeOneDArr(char* arr);
+int inputCount(int* count);
 
 int main() {
-    char str[] = "";
-    gets(str);
+    char* str = getStr();
+    if (str == NULL) {
+        outputText(MemoryError"\n");
+        _getch();
+        return !OK;
+    }
     outputText("|");
     outputText(str);
     outputText("|\n|");
-    solutionFunc(str);
-    outputText(str);
+    outputText(stdStr(str, &OneSpaceLeft, &OneSpaceRight));
     outputText("|");
+    freeOneDArr(str);
     _getch();
-    return OK;
-}
-
-int solutionFunc(char* str) {
-    int i = 0;
-    while (str[i]) {
-        if (str[i] == ' ' && (i == 0 || str[i+1]=='\0' || charInSyms(str[i + 1], OneSpaceRight))) {
-            delChar(str, i);
-            continue;
-        }else if(charInSyms(str[i], OneSpaceLeft) && str[i+1]==' '){
-            delChar(str, i+1);
-            continue;
-        }
-        i++;
-    }
     return OK;
 }
 
 int outputText(char* s) {
     printf(s);
     return OK;
+}
+
+void freeTwoDArr(char** matrix, int rows) {
+    for (int i = 0; i < rows; ++i) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+void freeOneDArr(char* arr) {
+    free(arr);
+}
+
+int inputCount(int* count) {
+    printf("Enter count: ");
+    return scanf("%i", count) == 0 || *count <= 0;
+}
+
+void* reallocList(void* str, int count, int sizeOfType) {
+    return realloc(str, sizeOfType * count);
+}
+
+void* mallocList(int count, int sizeOfType) {
+    return malloc(sizeOfType * count);
 }
